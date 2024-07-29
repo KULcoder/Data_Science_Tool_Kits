5bd9ee50f675f5685bc1d685c4990b3b58a0d83c
KULcoder <keyuucsd@gmail.com>
2024-07-29 21:59:59 +0800

    Update live_loss.py

--- src/utils/live_loss.py
+++ src/utils/live_loss.py
@@ -3,7 +3,15 @@ A class: display dynamic train acc and loss on command line.
 Useful for training deep learning models, and give you a sense about your training
 while not using big monitor libs like tensorboard.
 
-Design: Should hold a tqdm object, and being able to update it.
+requires: tqdm, numpy, pytorch
+
+Question:
+1. Do we want to add something to display live plot? 
+    Seems too complex and unnecessary
+2. Do we want to display something about validation loss?
+    Mechanism like early-stopping should be implemented outside this class, not
+    important to have
+
 
 How to use:
     1. Create this object with total number of epochs: live_loss = Live_loss(100)
@@ -14,17 +22,19 @@ How to use:
 """
 from tqdm import tqdm
 import numpy as np
+import torch
 
 class Live_loss:
     """
     Is designed to replace the epoch iterator.
     """
 
-    def __init__(self, total_epoch=100, display_interval=None):
+    def __init__(self, total_epoch=100, display_interval=1, gpu_usage=False):
         self.current_epoch = 1
-        self.step = 1
-        self.epochs = tqdm(range(total_epoch), desc=f"Epoch: {self.current_epoch}")
+        self.step = 0
+        self.epochs = tqdm(range(total_epoch))
         self.display_interval = display_interval
+        self.gpu_usage = gpu_usage
 
         self.live_losses = []
         self.live_accs = []
@@ -35,20 +45,23 @@ class Live_loss:
         self.live_losses.append(loss)
         self.live_accs.append(accuracy)
 
-        if len(self.live_accs) > 100:
-            self.live_accs.pop(0)
-        if len(self.live_losses) > 100:
-            self.live_losses.pop(0)
+        self.live_losses = self.live_losses[-100:]
+        self.live_accs = self.live_accs[-100:]
 
 
-        if self.display_interval and self.step % self.display_interval:
-            pass
-        else:
+        if self.display_interval and self.step % self.display_interval == 0:
             live_loss = np.round(np.mean(self.live_losses), 2)
-            live_acc = np.round(np.mean(self.live_accs), 2)
-            self.epochs.set_description(
-                f"Step: {self.step}, loss: {live_loss:.2f}, acc: {live_acc}"
-            )
+            live_acc = np.round(np.mean(self.live_accs) * 100, 2)
+            description = f"Step: {self.step}, loss: {live_loss:.2f}, acc: {live_acc}%"
+
+            if self.gpu_usage and torch.cuda.is_available:
+                gpu_memory_total = torch.cuda.get_device_properties(0).total_memory / 1024 ** 2
+                gpu_memory_reserved = torch.cuda.memory_reserved(0) / 1024 ** 2
+                gpu_memory_percentage = gpu_memory_reserved / gpu_memory_total
+                description += f", gpu_mem: {gpu_memory_percentage:.2f}%"
+
+            self.epochs.set_description(description)
+
 
 
     def finish_epoch(self):
